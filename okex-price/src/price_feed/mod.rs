dc40345b9b039d5527e532dc139bf23a632afabb
bodymindarts <justin@galoy.io>
2022-08-03 23:42:22 +0200

    fix: remove unwrap() calls

--- okex-price/src/price_feed/mod.rs
+++ okex-price/src/price_feed/mod.rs
@@ -8,21 +8,20 @@ pub use feeder::*;
 use futures::{SinkExt, Stream, StreamExt};
 use tokio_tungstenite::{connect_async, tungstenite::Message};
 
-use self::error::PriceFeedError;
+pub use error::PriceFeedError;
 
-#[allow(dead_code)]
 pub async fn subscribe_btc_usd_swap(
     config: PriceFeedConfig,
 ) -> Result<std::pin::Pin<Box<dyn Stream<Item = OkexPriceTick> + Send>>, PriceFeedError> {
-    let (ws_stream, _) = connect_async(config.url).await.unwrap();
+    let (ws_stream, _) = connect_async(config.url).await?;
     let (mut sender, receiver) = ws_stream.split();
 
     let subscribe_args = Subscribe::new();
-    let serialized_args = serde_json::to_string(&subscribe_args).unwrap();
+    let serialized_args = serde_json::to_string(&subscribe_args)?;
     let item = Message::Text(serialized_args);
 
     let subscribe = sender.send(item);
-    subscribe.await.unwrap();
+    subscribe.await?;
 
     Ok(Box::pin(receiver.filter_map(|message| async {
         if let Ok(msg) = message {
