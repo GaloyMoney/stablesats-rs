e7f5eb66d859949841021499c08c8d7e96025078
bodymindarts <justin@galoy.io>
2022-08-05 14:31:24 +0200

    fix: remove protobuf-src

--- price-server/build.rs
+++ price-server/build.rs
@@ -1,5 +1,4 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    std::env::set_var("PROTOC", protobuf_src::protoc());
     tonic_build::compile_protos("../proto/price/price.proto")?;
     Ok(())
 }
