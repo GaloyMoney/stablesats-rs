ed3972fccbb61dede0c78c2327ccc268271e48f1
bodymindarts <justin@galoy.io>
2022-08-06 11:01:39 +0200

    ci: remove publish step

--- ci/tasks/verify-publish.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cargo install --all-features stablesats
-if [[ $(stablesats --version) != "cepler $(cat version/version)" ]]; then
-  echo "Installed stablesats does not have expected version number"
-  exit 1
-fi
-stablesats help
