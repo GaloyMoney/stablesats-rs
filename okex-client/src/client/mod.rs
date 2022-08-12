28fbd1dad7505421f60d7286c33581f0ed3a3077
bodymindarts <justin@galoy.io>
2022-08-12 10:28:10 +0200

    refactor: move sign_okex_request into headers hepler

--- okex-client/src/client/mod.rs
+++ okex-client/src/client/mod.rs
@@ -51,11 +51,10 @@ impl OkexClient {
         let timestamp = Utc::now().to_rfc3339_opts(SecondsFormat::Millis, true);
         let pre_hash = format!("{}GET{}", timestamp, request_path);
 
-        let sign_base64 = self.sign_okex_request(pre_hash);
-
-        let headers = self.request_headers(timestamp.as_str(), sign_base64)?;
+        let headers = self.request_headers(timestamp.as_str(), pre_hash)?;
 
         let response = self.client.get(url).headers(headers).send().await?;
+
         let response_text = response.text().await?;
 
         let response = match serde_json::from_str::<OkexResponse>(&response_text)? {
@@ -97,9 +96,7 @@ impl OkexClient {
         let timestamp = Utc::now().to_rfc3339_opts(SecondsFormat::Millis, true);
         let pre_hash = format!("{}POST{}{}", timestamp, request_path, request_body);
 
-        let sign_base64 = self.sign_okex_request(pre_hash);
-
-        let headers = self.request_headers(timestamp.as_str(), sign_base64)?;
+        let headers = self.request_headers(timestamp.as_str(), pre_hash)?;
 
         let response = self
             .client
@@ -108,6 +105,7 @@ impl OkexClient {
             .body(request_body)
             .send()
             .await?;
+
         let response_text = response.text().await?;
 
         let response = match serde_json::from_str::<OkexResponse>(&response_text)? {
@@ -144,15 +142,20 @@ impl OkexClient {
     fn request_headers(
         &self,
         formatted_timestamp: &str,
-        signed: String,
+        pre_hash: String,
     ) -> Result<HeaderMap, OkexClientError> {
+        let sign_base64 = self.sign_okex_request(pre_hash);
+
         let mut headers = HeaderMap::new();
         headers.insert(CONTENT_TYPE, HeaderValue::from_str("application/json")?);
         headers.insert(
             "OK-ACCESS-KEY",
             HeaderValue::from_str(self.config.api_key.as_str())?,
         );
-        headers.insert("OK-ACCESS-SIGN", HeaderValue::from_str(signed.as_str())?);
+        headers.insert(
+            "OK-ACCESS-SIGN",
+            HeaderValue::from_str(sign_base64.as_str())?,
+        );
         headers.insert(
             "OK-ACCESS-TIMESTAMP",
             HeaderValue::from_str(formatted_timestamp)?,
