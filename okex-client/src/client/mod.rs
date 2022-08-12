05ac091c00be54f470ed5603df7fc741c762eac0
bodymindarts <justin@galoy.io>
2022-08-12 09:32:46 +0200

    refactor: extract common header creation

--- okex-client/src/client/mod.rs
+++ okex-client/src/client/mod.rs
@@ -53,23 +53,7 @@ impl OkexClient {
 
         let sign_base64 = self.sign_okex_request(pre_hash);
 
-        let mut headers = HeaderMap::new();
-        headers.insert(
-            "OK-ACCESS-KEY",
-            HeaderValue::from_str(self.config.api_key.as_str())?,
-        );
-        headers.insert(
-            "OK-ACCESS-SIGN",
-            HeaderValue::from_str(sign_base64.as_str())?,
-        );
-        headers.insert(
-            "OK-ACCESS-TIMESTAMP",
-            HeaderValue::from_str(timestamp.as_str())?,
-        );
-        headers.insert(
-            "OK-ACCESS-PASSPHRASE",
-            HeaderValue::from_str(self.config.passphrase.as_str())?,
-        );
+        let headers = self.request_headers(timestamp.as_str(), sign_base64)?;
 
         let response = self.client.get(url).headers(headers).send().await?;
         let response_text = response.text().await?;
@@ -112,26 +96,10 @@ impl OkexClient {
 
         let timestamp = Utc::now().to_rfc3339_opts(SecondsFormat::Millis, true);
         let pre_hash = format!("{}POST{}{}", timestamp, request_path, request_body);
+
         let sign_base64 = self.sign_okex_request(pre_hash);
 
-        let mut headers = HeaderMap::new();
-        headers.insert(CONTENT_TYPE, HeaderValue::from_str("application/json")?);
-        headers.insert(
-            "OK-ACCESS-KEY",
-            HeaderValue::from_str(self.config.api_key.as_str())?,
-        );
-        headers.insert(
-            "OK-ACCESS-SIGN",
-            HeaderValue::from_str(sign_base64.as_str())?,
-        );
-        headers.insert(
-            "OK-ACCESS-TIMESTAMP",
-            HeaderValue::from_str(timestamp.as_str())?,
-        );
-        headers.insert(
-            "OK-ACCESS-PASSPHRASE",
-            HeaderValue::from_str(self.config.passphrase.as_str())?,
-        );
+        let headers = self.request_headers(timestamp.as_str(), sign_base64)?;
 
         let response = self
             .client
@@ -171,5 +139,28 @@ impl OkexClient {
         let key = hmac::Key::new(hmac::HMAC_SHA256, self.config.secret_key.as_bytes());
         let signature = hmac::sign(&key, pre_hash.as_bytes());
         BASE64.encode(signature.as_ref())
+    }
+
+    fn request_headers(
+        &self,
+        formatted_timestamp: &str,
+        signed: String,
+    ) -> Result<HeaderMap, OkexClientError> {
+        let mut headers = HeaderMap::new();
+        headers.insert(CONTENT_TYPE, HeaderValue::from_str("application/json")?);
+        headers.insert(
+            "OK-ACCESS-KEY",
+            HeaderValue::from_str(self.config.api_key.as_str())?,
+        );
+        headers.insert("OK-ACCESS-SIGN", HeaderValue::from_str(signed.as_str())?);
+        headers.insert(
+            "OK-ACCESS-TIMESTAMP",
+            HeaderValue::from_str(formatted_timestamp)?,
+        );
+        headers.insert(
+            "OK-ACCESS-PASSPHRASE",
+            HeaderValue::from_str(self.config.passphrase.as_str())?,
+        );
+        Ok(headers)
     }
 }
