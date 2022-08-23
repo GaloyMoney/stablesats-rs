38325b32c7c71192b4cd6bc4b20595e82083b564
bodymindarts <justin@galoy.io>
2022-08-23 16:30:32 +0200

    refactor: make new-balance.sql more efficient

--- user-trades/src/user_trade_balances/sql/new-balance.sql
+++ user-trades/src/user_trade_balances/sql/new-balance.sql
@@ -1,13 +1,13 @@
- WITH buy_amounts AS (
-   SELECT MAX(idx) as max_buy, buy_unit, SUM(buy_amount) AS to_sub FROM user_trades WHERE idx > $1 GROUP BY buy_unit
- ),
- sell_amounts AS (
-   SELECT MAX(idx) as max_sell, sell_unit, SUM(sell_amount) AS to_add FROM user_trades WHERE idx > $1 GROUP BY sell_unit
+ WITH amounts AS (
+   SELECT MAX(idx) as latest_idx, buy_unit, SUM(buy_amount) AS to_sub, sell_unit, SUM(sell_amount) AS to_add 
+   FROM user_trades
+   WHERE idx > $1
+   GROUP BY GROUPING SETS ((buy_unit), (sell_unit))
  )
 SELECT
   unit AS "unit: _",
-  current_balance + COALESCE(to_add, 0) - COALESCE(to_sub, 0) AS new_balance,
-  MAX(GREATEST(max_buy, max_sell)) OVER () AS new_latest_idx
+  current_balance + COALESCE(sell_amounts.to_add, 0) - COALESCE(buy_amounts.to_sub, 0) AS new_balance,
+  MAX(GREATEST(sell_amounts.latest_idx, buy_amounts.latest_idx)) OVER () AS new_latest_idx
 FROM user_trade_balances
-LEFT JOIN buy_amounts ON unit=buy_unit
-LEFT JOIN sell_amounts ON unit=sell_unit;
+LEFT JOIN amounts buy_amounts ON unit=buy_amounts.buy_unit
+LEFT JOIN amounts sell_amounts ON unit=sell_amounts.sell_unit;
