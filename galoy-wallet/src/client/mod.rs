2acf0e8613e15dd6edc082cce9a79a9441c97dc7
Enigbe Ochekliye <ochekliyeenigbe@gmail.com>
2022-08-24 20:25:40 +0100

    feat: send auth code to wallet phone number

--- galoy-wallet/src/client/mod.rs
+++ galoy-wallet/src/client/mod.rs
@@ -4,34 +4,51 @@ use error::*;
 use graphql_client::{reqwest::post_graphql, GraphQLQuery};
 use reqwest::Client as ReqwestClient;
 
+use auth_code::*;
 use btc_price::*;
 
 #[derive(GraphQLQuery)]
 #[graphql(
     schema_path = "src/client/graphql/schema.graphql",
-    query_path = "src/client/graphql/query_btc_price.graphql",
+    query_path = "src/client/graphql/queries/btc_price.graphql",
     response_derives = "Debug, PartialEq"
 )]
 pub struct BtcPrice;
 type SafeInt = i64;
 
+#[derive(GraphQLQuery)]
+#[graphql(
+    schema_path = "src/client/graphql/schema.graphql",
+    query_path = "src/client/graphql/mutations/user_request_auth_code.graphql",
+    response_derives = "Debug, PartialEq"
+)]
+pub struct AuthCode;
+type Phone = String;
+
+#[derive(Debug, Clone)]
 pub struct GaloyClient {
     client: ReqwestClient,
+    config: GaloyClientConfig,
+}
 
-    api: String,
+#[derive(Debug, Clone)]
+pub struct GaloyClientConfig {
+    pub api: String,
+    pub phone_number: String,
 }
 
 impl GaloyClient {
-    pub fn new(api: String) -> Self {
+    pub fn new(config: GaloyClientConfig) -> Self {
         Self {
             client: ReqwestClient::new(),
-            api,
+            config,
         }
     }
 
     pub async fn btc_price(&self) -> Result<BtcPriceBtcPrice, GaloyWalletError> {
         let variables = btc_price::Variables;
-        let response = post_graphql::<BtcPrice, _>(&self.client, &self.api, variables).await?;
+        let response =
+            post_graphql::<BtcPrice, _>(&self.client, &self.config.api, variables).await?;
         let response_data = response.data;
 
         if let Some(response_data) = response_data {
@@ -39,6 +56,31 @@ impl GaloyClient {
             if let Some(result) = result {
                 return Ok(result);
             }
+        }
+        Err(GaloyWalletError::UnknownResponse(
+            "Failed to parse response data".to_string(),
+        ))
+    }
+
+    pub async fn authentication_code(
+        &self,
+    ) -> Result<AuthCodeUserRequestAuthCode, GaloyWalletError> {
+        let phone_number = auth_code::Variables {
+            input: auth_code::UserRequestAuthCodeInput {
+                phone: self.config.phone_number.clone(),
+            },
+        };
+        let response =
+            post_graphql::<AuthCode, _>(&self.client, &self.config.phone_number, phone_number)
+                .await?;
+        println!("{:?}", response);
+        let response_data = response.data;
+        println!("{:?}", response_data);
+
+        if let Some(response_data) = response_data {
+            let result = response_data.user_request_auth_code;
+
+            return Ok(result);
         }
         Err(GaloyWalletError::UnknownResponse(
             "Failed to parse response data".to_string(),
--- galoy-wallet/tests/galoy_wallet.rs
+++ galoy-wallet/tests/galoy_wallet.rs
@@ -1,9 +1,14 @@
 use galoy_wallet::*;
+use std::env;
 
 /// Test send an non-authenticated query to the Galoy Graphql API
 #[tokio::test]
 async fn get_btc_price() -> anyhow::Result<()> {
-    let wallet = GaloyClient::new("http://localhost:4002/graphql".to_string());
+    let api = env::var("GALOY_GRAPH_URI").expect("GALOY_GRAPH_URI not set");
+    let phone_number = env::var("WALLET_PHONE_NUMBER").expect("WALLET_PHONE_NUMBER not set");
+    let config = GaloyClientConfig { api, phone_number };
+
+    let wallet = GaloyClient::new(config);
     let price = wallet.btc_price().await?;
 
     println!("{:#?}", price);
@@ -12,6 +17,29 @@ async fn get_btc_price() -> anyhow::Result<()> {
     assert_eq!(
         price.currency_unit,
         btc_price::ExchangeCurrencyUnit::USDCENT
+    );
+
+    Ok(())
+}
+
+/// Get an authentication code from the Galoy Graphql API
+#[tokio::test]
+async fn authentication_code() -> anyhow::Result<()> {
+    let api = env::var("GALOY_GRAPH_URI").expect("GALOY_GRAPH_URI not set");
+    let phone_number = env::var("WALLET_PHONE_NUMBER").expect("WALLET_PHONE_NUMBER not set");
+    let config = GaloyClientConfig { api, phone_number };
+
+    let wallet = GaloyClient::new(config);
+    let auth_code_response = wallet.authentication_code().await?;
+
+    assert_eq!(
+        auth_code_response,
+        auth_code::AuthCodeUserRequestAuthCode {
+            success: Some(true),
+            errors: vec![auth_code::AuthCodeUserRequestAuthCodeErrors {
+                message: "".to_string()
+            }]
+        }
     );
 
     Ok(())
