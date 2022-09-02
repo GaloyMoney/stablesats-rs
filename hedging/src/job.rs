59b2a517e4a4b41bae278448aec4f626ab94802c
bodymindarts <justin@galoy.io>
2022-09-02 13:14:11 +0200

    fix: serialize insert-if-new

--- hedging/src/job.rs
+++ hedging/src/job.rs
@@ -2,7 +2,10 @@ use sqlxmq::{job, CurrentJob, JobRegistry, OwnedHandle};
 
 use crate::{error::*, synth_usd_liability::*};
 
-pub async fn start_job_runner(pool: sqlx::PgPool, synth_usd_liability: SynthUsdLiability) -> Result<OwnedHandle, HedgingError> {
+pub async fn start_job_runner(
+    pool: sqlx::PgPool,
+    synth_usd_liability: SynthUsdLiability,
+) -> Result<OwnedHandle, HedgingError> {
     let mut registry = JobRegistry::new(&[adjust_hedge]);
     registry.set_context(synth_usd_liability);
 
@@ -19,13 +22,16 @@ pub async fn spawn_adjust_hedge(pool: &sqlx::PgPool) -> Result<(), HedgingError>
 }
 
 #[job(name = "adjust_hedge", channel_name = "hedging")]
-pub async fn adjust_hedge(mut current_job: CurrentJob, synth_usd_liability: SynthUsdLiability) -> Result<(), HedgingError> {
+pub async fn adjust_hedge(
+    mut current_job: CurrentJob,
+    synth_usd_liability: SynthUsdLiability,
+) -> Result<(), HedgingError> {
     let latest_liability = synth_usd_liability.get_latest_liability().await?;
     // use OKEX client here
     // load last known exposure
     // if needed {
-      // execute hedge adjustment
-      // => if fail then fail job
+    // execute hedge adjustment
+    // => if fail then fail job
     // }
 
     current_job.complete().await?;
--- hedging/src/synth_usd_liability/mod.rs
+++ hedging/src/synth_usd_liability/mod.rs
@@ -1,4 +1,5 @@
 use rust_decimal::Decimal;
+use sqlx::{Executor, PgPool};
 use uuid::Uuid;
 
 use shared::pubsub::CorrelationId;
@@ -7,11 +8,11 @@ use crate::error::HedgingError;
 
 #[derive(Clone)]
 pub struct SynthUsdLiability {
-    pool: sqlx::PgPool,
+    pool: PgPool,
 }
 
 impl SynthUsdLiability {
-    pub fn new(pool: sqlx::PgPool) -> Self {
+    pub fn new(pool: PgPool) -> Self {
         Self { pool }
     }
 
@@ -20,21 +21,28 @@ impl SynthUsdLiability {
         correlation_id: CorrelationId,
         amount: Decimal,
     ) -> Result<bool, HedgingError> {
+        let mut tx = self.pool.begin().await?;
+        tx.execute("SET TRANSACTION ISOLATION LEVEL SERIALIZABLE;")
+            .await?;
+
         let result = sqlx::query_file!(
             "src/synth_usd_liability/sql/insert-if-new.sql",
             amount,
             Uuid::from(correlation_id)
         )
-        .fetch_all(&self.pool)
+        .fetch_all(&mut tx)
         .await?;
+
+        tx.commit().await?;
 
         Ok(!result.is_empty())
     }
 
     pub async fn get_latest_liability(&self) -> Result<Decimal, HedgingError> {
-        let result = sqlx::query!("SELECT amount FROM synth_usd_liability ORDER BY idx DESC LIMIT 1")
-            .fetch_one(&self.pool)
-            .await?;
+        let result =
+            sqlx::query!("SELECT amount FROM synth_usd_liability ORDER BY idx DESC LIMIT 1")
+                .fetch_one(&self.pool)
+                .await?;
 
         Ok(result.amount)
     }
