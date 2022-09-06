d2806e8473f3ab6a90db96bbe6508362ad030b57
Sebastien Verreault <sebver@pm.me>
2022-09-06 16:52:19 +0900

    feat: adding hysteresis around hedging actions

--- hedging/src/adjustment_action.rs
+++ hedging/src/adjustment_action.rs
@@ -1,9 +1,15 @@
 use rust_decimal::Decimal;
+use rust_decimal_macros::dec;
 
 pub use okex_client::BtcUsdSwapContracts;
 
-const CONTRACT_SIZE: u32 = 100;
-const MIN_LIABILITY_THRESHOLD: i32 = -95;
+const CONTRACT_SIZE: Decimal = dec!(100);
+const MIN_LIABILITY_THRESHOLD: Decimal = dec!(100); // CONTRACT_SIZE / 2
+
+const LOW_BOUND_RATIO_SHORTING: Decimal = dec!(0.95);
+const LOW_SAFEBOUND_RATIO_SHORTING: Decimal = dec!(0.98);
+const HIGH_SAFEBOUND_RATIO_SHORTING: Decimal = dec!(1.);
+const HIGH_BOUND_RATIO_SHORTING: Decimal = dec!(1.03);
 
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub enum AdjustmentAction {
@@ -53,35 +59,34 @@ impl AdjustmentAction {
     }
 }
 
-pub fn determine_action(abs_liability: Decimal, signed_exposure: Decimal) -> AdjustmentAction {
-    let target_exposure = abs_liability * Decimal::NEGATIVE_ONE;
-    if target_exposure > Decimal::from(MIN_LIABILITY_THRESHOLD) && signed_exposure != Decimal::ZERO
-    {
+pub fn determine_action(abs_liability: Decimal, abs_exposure: Decimal) -> AdjustmentAction {
+    if abs_liability >= Decimal::ZERO && abs_liability < MIN_LIABILITY_THRESHOLD {
         AdjustmentAction::ClosePosition
-    } else if target_exposure > signed_exposure {
-        let contracts = ((signed_exposure - target_exposure) / Decimal::from(CONTRACT_SIZE))
-            .round()
-            .abs();
-        if contracts == Decimal::ZERO {
-            AdjustmentAction::DoNothing
-        } else {
-            AdjustmentAction::Buy(BtcUsdSwapContracts::from(
-                u32::try_from(contracts).expect("decimal to u32"),
-            ))
-        }
-    } else if target_exposure < signed_exposure {
-        let contracts = ((target_exposure - signed_exposure) / Decimal::from(CONTRACT_SIZE))
-            .round()
-            .abs();
-        if contracts == Decimal::ZERO {
-            AdjustmentAction::DoNothing
-        } else {
-            AdjustmentAction::Sell(BtcUsdSwapContracts::from(
-                u32::try_from(contracts).expect("decimal to u32"),
-            ))
-        }
     } else {
-        AdjustmentAction::DoNothing
+        let exposure_ratio = abs_exposure / abs_liability;
+        if exposure_ratio < LOW_BOUND_RATIO_SHORTING {
+            let target_exposure = abs_liability * LOW_SAFEBOUND_RATIO_SHORTING;
+            let contracts = ((target_exposure - abs_exposure) / CONTRACT_SIZE).round();
+            if contracts == Decimal::ZERO {
+                AdjustmentAction::DoNothing
+            } else {
+                AdjustmentAction::Sell(BtcUsdSwapContracts::from(
+                    u32::try_from(contracts).expect("decimal to u32"),
+                ))
+            }
+        } else if exposure_ratio > HIGH_BOUND_RATIO_SHORTING {
+            let target_exposure = abs_liability * HIGH_SAFEBOUND_RATIO_SHORTING;
+            let contracts = ((abs_exposure - target_exposure) / CONTRACT_SIZE).round();
+            if contracts == Decimal::ZERO {
+                AdjustmentAction::DoNothing
+            } else {
+                AdjustmentAction::Buy(BtcUsdSwapContracts::from(
+                    u32::try_from(contracts).expect("decimal to u32"),
+                ))
+            }
+        } else {
+            AdjustmentAction::DoNothing
+        }
     }
 }
 
@@ -93,7 +98,7 @@ mod tests {
     #[test]
     fn no_adjustment() {
         let liability = dec!(100);
-        let exposure = dec!(-100);
+        let exposure = dec!(100);
         let adjustment = determine_action(liability, exposure);
         assert_eq!(adjustment, AdjustmentAction::DoNothing);
     }
@@ -101,7 +106,7 @@ mod tests {
     #[test]
     fn close_position() {
         let liability = dec!(0);
-        let exposure = dec!(-100);
+        let exposure = dec!(100);
         let adjustment = determine_action(liability, exposure);
         assert_eq!(adjustment, AdjustmentAction::ClosePosition);
     }
@@ -109,7 +114,7 @@ mod tests {
     #[test]
     fn increase() {
         let liability = dec!(200);
-        let exposure = dec!(-100);
+        let exposure = dec!(100);
         let adjustment = determine_action(liability, exposure);
         assert_eq!(
             adjustment,
@@ -120,7 +125,7 @@ mod tests {
     #[test]
     fn decrease() {
         let liability = dec!(1000);
-        let exposure = dec!(-5998.824959074429);
+        let exposure = dec!(5998.824959074429);
         let adjustment = determine_action(liability, exposure);
         assert_eq!(
             adjustment,
@@ -131,7 +136,7 @@ mod tests {
     #[test]
     fn ignores_rounding() {
         let liability = dec!(100);
-        let exposure = dec!(-99.8);
+        let exposure = dec!(99.8);
         let adjustment = determine_action(liability, exposure);
         assert_eq!(adjustment, AdjustmentAction::DoNothing);
     }
