0748c7b3f05288b41bbe2b696c7bdec14055660f
bodymindarts <justin@galoy.io>
2022-09-13 10:19:37 +0200

    fix: deserialize Duration with serde_with

--- hedging/src/app/config.rs
+++ hedging/src/app/config.rs
@@ -1,12 +1,14 @@
 use serde::{Deserialize, Serialize};
 use std::time::Duration;
 
+#[serde_with::serde_as]
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct HedgingAppConfig {
     #[serde(default)]
     pub pg_con: String,
     #[serde(default = "bool_true")]
     pub migrate_on_start: bool,
+    #[serde_as(as = "serde_with::DurationSeconds<u64>")]
     #[serde(default = "default_okex_poll_frequency")]
     pub okex_poll_frequency: Duration,
 }
--- user-trades/src/app/config.rs
+++ user-trades/src/app/config.rs
@@ -1,14 +1,17 @@
 use serde::{Deserialize, Serialize};
 use std::time::Duration;
 
+#[serde_with::serde_as]
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct UserTradesConfig {
     #[serde(default)]
     pub pg_con: String,
     #[serde(default = "bool_true")]
     pub migrate_on_start: bool,
+    #[serde_as(as = "serde_with::DurationSeconds<u64>")]
     #[serde(default = "default_balance_publish_frequency")]
     pub balance_publish_frequency: Duration,
+    #[serde_as(as = "serde_with::DurationSeconds<u64>")]
     #[serde(default = "default_galoy_poll_frequency")]
     pub galoy_poll_frequency: Duration,
 }
