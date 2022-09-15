24d70455dd18de153e9101a89d58a6510010f108
bodymindarts <justin@galoy.io>
2022-09-15 14:12:26 +0200

    fix: more robust listen to user trades notification

--- user-trades/src/user_trade_balances/mod.rs
+++ user-trades/src/user_trade_balances/mod.rs
@@ -1,6 +1,6 @@
 use rust_decimal::Decimal;
 use sqlx::{postgres::PgListener, PgPool};
-use tracing::instrument;
+use tracing::{info_span, instrument, Instrument};
 
 use std::collections::HashMap;
 
@@ -27,17 +27,67 @@ pub struct UserTradeBalances {
 
 impl UserTradeBalances {
     pub async fn new(pool: PgPool, units: UserTradeUnits) -> Result<Self, UserTradesError> {
-        let mut listener = PgListener::connect_with(&pool).await?;
-        listener.listen("user_trades").await?;
-        let ret = Self { pool, units };
+        let ret = Self {
+            pool: pool.clone(),
+            units,
+        };
         let user_trade_balances = ret.clone();
         tokio::spawn(async move {
-            while listener.recv().await.is_ok() {
-                let _ = user_trade_balances.update_balances().await;
+            loop {
+                let _ = Self::listen_to_events(pool.clone(), user_trade_balances.clone()).await;
+                tokio::time::sleep(std::time::Duration::from_secs(5)).await;
             }
         });
-        ret.update_balances().await?;
         Ok(ret)
+    }
+
+    async fn listen_to_events(
+        pool: PgPool,
+        user_trade_balances: Self,
+    ) -> Result<(), UserTradesError> {
+        let span = info_span!(
+            "spawn_listen_to_user_trades_notifications",
+            error = tracing::field::Empty,
+            error.message = tracing::field::Empty,
+        );
+        let (send, recv) = tokio::sync::oneshot::channel();
+        shared::tracing::record_error::<(), UserTradesError, _, _>(|| async move {
+            let mut listener = PgListener::connect_with(&pool).await?;
+            listener.listen("user_trades").await?;
+            user_trade_balances.update_balances().await?;
+            tokio::spawn(async move {
+                loop {
+                    match listener.recv().await {
+                        Ok(_) => {
+                            let span = info_span!(
+                                "user_trades_notification_received",
+                                error = tracing::field::Empty,
+                                error.message = tracing::field::Empty,
+                            );
+                            let repo = user_trade_balances.clone();
+                            if let Err(e) = shared::tracing::record_error(|| async move {
+                                repo.update_balances().await
+                            })
+                            .instrument(span)
+                            .await
+                            {
+                                let _ = send.send(e);
+                                break;
+                            }
+                        }
+                        Err(e) => {
+                            let _ = send.send(e.into());
+                            break;
+                        }
+                    }
+                }
+            });
+            Ok(())
+        })
+        .instrument(span)
+        .await?;
+        let _ = recv.await;
+        Ok(())
     }
 
     #[instrument(name = "update_user_trade_balances", skip(self),
