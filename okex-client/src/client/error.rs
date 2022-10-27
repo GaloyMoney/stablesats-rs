f2ea8fb43234516bf9c44430b42907f657a1c8dc
Sebastien Verreault <sebver@pm.me>
2022-10-27 17:46:32 +0900

    chore: add transfers specific error handling

--- okex-client/src/client/error.rs
+++ okex-client/src/client/error.rs
@@ -14,6 +14,10 @@ pub enum OkexClientError {
     ServiceUnavailable { msg: String, code: String },
     #[error("OkexClientError - OrderDoesNotExist")]
     OrderDoesNotExist,
+    #[error("OkexClientError - ParameterClientIdError")]
+    ParameterClientIdError,
+    #[error("OkexClientError - WithdrawalIdDoesNotExist")]
+    WithdrawalIdDoesNotExist,
     #[error("OkexClientError - DecimalConversion: {0}")]
     DecimalConversion(#[from] rust_decimal::Error),
     #[error("OkexClientError - MisconfiguredAccount: {0}")]
@@ -24,7 +28,9 @@ impl From<(String, String)> for OkexClientError {
     fn from((msg, code): (String, String)) -> Self {
         match code.as_str() {
             "50001" => OkexClientError::ServiceUnavailable { msg, code },
+            "51000" => OkexClientError::ParameterClientIdError,
             "51603" => OkexClientError::OrderDoesNotExist,
+            "58215" => OkexClientError::WithdrawalIdDoesNotExist,
             _ => OkexClientError::UnexpectedResponse { msg, code },
         }
     }
