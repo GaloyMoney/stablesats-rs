b9eeb8ae48c10dbb253ffe54c17152d31edceca5
ngutech21 <ngutech21@pm.me>
2022-10-28 09:27:49 +0200

    add test for publishing to redis

--- kollider-price/src/convert.rs
+++ kollider-price/src/convert.rs
@@ -14,8 +14,8 @@ impl TryFrom<KolliderPriceTicker> for KolliderBtcUsdSwapPricePayload {
     fn try_from(value: KolliderPriceTicker) -> Result<Self, Self::Error> {
         Ok(KolliderBtcUsdSwapPricePayload(PriceMessagePayload {
             exchange: ExchangeIdRaw::from("Kollider"), // FIXME
-            instrument_id: InstrumentIdRaw::from(value.symbol),
-            timestamp: TimeStamp::now(), //FIXME
+            instrument_id: InstrumentIdRaw::from("BTC-USD-SWAP"), // FIXME "BTC-USD-SWAP"
+            timestamp: TimeStamp::now(),               //FIXME
             ask_price: PriceRatioRaw::from_one_btc_in_usd_price(value.best_ask),
             bid_price: PriceRatioRaw::from_one_btc_in_usd_price(value.best_bid),
         }))
