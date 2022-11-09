438ee8753ed2fa75f6066a12e68082572bcb5ee2
ngutech21 <ngutech21@pm.me>
2022-11-09 14:44:42 +0100

    feat: add KolliderMarginType

--- kollider-client/src/client/primitives.rs
+++ kollider-client/src/client/primitives.rs
@@ -125,3 +125,18 @@ impl Display for KolliderOrderSide {
         }
     }
 }
+
+#[derive(Debug, Clone)]
+pub enum KolliderMarginType {
+    Cross,
+    Isolated,
+}
+
+impl Display for KolliderMarginType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            KolliderMarginType::Cross => write!(f, "Cross"),
+            KolliderMarginType::Isolated => write!(f, "Isolated"),
+        }
+    }
+}
