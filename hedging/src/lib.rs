a4ce74bfd2135c9426d81ca7e87d70e5a7d74612
Sebastien Verreault <sebver@pm.me>
2023-01-06 21:03:04 +0900

    chore: propagate bitfinex config for hedging

--- hedging/src/lib.rs
+++ hedging/src/lib.rs
@@ -11,7 +11,12 @@ mod rebalance_action;
 mod synth_usd_liability;
 
 use galoy_client::GaloyClientConfig;
-use shared::{exchanges_config::OkexConfig, health::HealthCheckTrigger, payload::*, pubsub::*};
+use shared::{
+    exchanges_config::{BitfinexConfig, OkexConfig},
+    health::HealthCheckTrigger,
+    payload::*,
+    pubsub::*,
+};
 
 pub use app::*;
 pub use error::*;
@@ -20,6 +25,7 @@ pub async fn run(
     health_check_trigger: HealthCheckTrigger,
     config: HedgingAppConfig,
     okex_config: OkexConfig,
+    bitfinex_config: BitfinexConfig,
     galoy_config: GaloyClientConfig,
     pubsub_cfg: PubSubConfig,
     tick_receiver: memory::Subscriber<PriceStreamPayload>,
@@ -28,6 +34,7 @@ pub async fn run(
         health_check_trigger,
         config,
         okex_config,
+        bitfinex_config,
         galoy_config,
         pubsub_cfg,
         tick_receiver,
