9fe109b74d74dc167f4ab8600745c7c60919bb93
Kartik Shah <krtk.6160@gmail.com>
2023-05-09 00:21:50 +0530

    chore: rename proto file (#370)

--- price-server/build.rs
+++ price-server/build.rs
@@ -1,5 +1,5 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     std::env::set_var("PROTOC", protobuf_src::protoc());
-    tonic_build::compile_protos("../proto/price/price.proto")?;
+    tonic_build::compile_protos("../proto/price/price_service.proto")?;
     Ok(())
 }
