dc2427b8f96aaacef295e75f431e9c139745757d
Seb Ver <sebver@pop-os.localdomain>
2023-06-16 17:52:55 +0900

    fix: migration column default to now

--- migrations/20230608050451_galoy-transactions-unpaired-last-check-at.up.sql
+++ migrations/20230608050451_galoy-transactions-unpaired-last-check-at.up.sql
@@ -1,2 +1,2 @@
-ALTER TABLE galoy_transactions ADD COLUMN unpaired_last_checked_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT clock_timestamp();
+ALTER TABLE galoy_transactions ADD COLUMN unpaired_last_checked_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW();
 UPDATE galoy_transactions set unpaired_last_checked_at = created_at;
