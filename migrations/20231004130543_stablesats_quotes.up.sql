81550fb56ddb9601a0ea6a3c36fd57d718f0b707
Vaibhav <thevaibhavdixit@gmail.com>
2023-10-25 12:53:23 +0530

    fix: rename stablesats_quote -> stablesats_quotes

--- migrations/20231004130543_stablesats_quotes.up.sql
+++ migrations/20231004130543_stablesats_quotes.up.sql
@@ -1,11 +1,12 @@
 CREATE TYPE direction_enum AS ENUM ('BuyCents', 'SellCents');
 
-CREATE TABLE stablesats_quote (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid()
+CREATE TABLE stablesats_quotes (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE stablesats_quote_events (
-    id UUID REFERENCES stablesats_quote(id) NOT NULL,
+    id UUID REFERENCES stablesats_quotes(id) NOT NULL,
     sequence INT NOT NULL,
     event_type VARCHAR NOT NULL,
     event JSONB NOT NULL,
