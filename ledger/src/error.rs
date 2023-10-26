f45cf9919c2582a2631ccb4fd1ecbf6de54fc4eb
Vaibhav <thevaibhavdixit@gmail.com>
2023-10-27 01:09:56 +0530

    refactor: remove redundant errors

--- ledger/src/error.rs
+++ ledger/src/error.rs
@@ -5,12 +5,4 @@ use thiserror::Error;
 pub enum LedgerError {
     #[error("LedgerError - SqlxLedger: {0}")]
     SqlxLedger(#[from] sqlx_ledger::SqlxLedgerError),
-    #[error("LedgerError - Transaction not found")]
-    TransactionNotFound,
-    #[error("LedgerError - MissingTxMetadata")]
-    MissingTxMetadata,
-    #[error("LedgerError - NotFound: {0}")]
-    ExpectedEntryNotFoundInTx(&'static str),
-    #[error("LedgerError - SerdeJson: {0}")]
-    SerdeJson(#[from] serde_json::Error),
 }
