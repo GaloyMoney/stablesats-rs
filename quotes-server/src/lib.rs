4733c12e58dd1dc1ce30ec0d5d3f81ba9d5384c2
Vaibhav <thevaibhavdixit@gmail.com>
2023-11-14 23:59:29 +0530

    feat: create run fn

--- quotes-server/src/lib.rs
+++ quotes-server/src/lib.rs
@@ -1,12 +1,48 @@
 #![cfg_attr(feature = "fail-on-warnings", deny(warnings))]
 #![cfg_attr(feature = "fail-on-warnings", deny(clippy::all))]
 
+pub mod app;
 pub mod cache;
 pub mod currency;
 pub mod entity;
+pub mod error;
 pub mod price;
 pub mod quote;
+pub mod server;
 
+use shared::{health::HealthCheckTrigger, payload::*, pubsub::memory};
+
+pub use app::*;
 pub use cache::QuotesExchangePriceCacheConfig;
 pub use entity::*;
 pub use price::*;
+pub use server::*;
+
+#[allow(clippy::too_many_arguments)]
+pub async fn run(
+    health_check_trigger: HealthCheckTrigger,
+    health_check_cfg: QuotesServerHealthCheckConfig,
+    server_config: QuotesServerConfig,
+    fee_calc_cfg: QuotesFeeCalculatorConfig,
+    subscriber: memory::Subscriber<PriceStreamPayload>,
+    price_cache_config: QuotesExchangePriceCacheConfig,
+    exchange_weights: ExchangeWeights,
+    pool: sqlx::PgPool,
+    quotes_config: QuotesConfig,
+) -> Result<(), QuotesServerError> {
+    let app = QuotesApp::run(
+        health_check_trigger,
+        health_check_cfg,
+        fee_calc_cfg,
+        subscriber,
+        price_cache_config,
+        exchange_weights,
+        quotes_config,
+        pool,
+    )
+    .await?;
+
+    server::start(server_config, app).await?;
+
+    Ok(())
+}
