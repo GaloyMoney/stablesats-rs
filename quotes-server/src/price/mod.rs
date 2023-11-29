488a7da6a1f70bcd1783ab872008a12ad7aa0dc9
Vaibhav <thevaibhavdixit@gmail.com>
2023-11-29 18:47:52 +0530

    chore: improve upon the existing rounding logic

--- quotes-server/src/price/mod.rs
+++ quotes-server/src/price/mod.rs
@@ -16,6 +16,7 @@ pub use mixer::*;
 pub use tick_converter::*;
 pub use traits::*;
 
+#[derive(Debug)]
 pub struct ConversionResult {
     pub sats: Satoshis,
     pub cents: UsdCents,
@@ -44,12 +45,13 @@ impl PriceCalculator {
         let cents = self
             .price_mixer
             .apply(|p| p.buy_usd().cents_from_sats(sats))
-            .await?;
+            .await?
+            .floor();
         let cents_after_fee = self
             .fee_calculator
             .decrease_by_fee(immediate_execution, cents)
             .floor();
-        let cents_spread = (cents_after_fee - cents).floor();
+        let cents_spread = cents_after_fee - cents;
         let sats_spread = sats_spread(sats, cents, cents_after_fee);
         Ok(ConversionResult {
             sats,
@@ -67,12 +69,13 @@ impl PriceCalculator {
         let sats = self
             .price_mixer
             .apply(|p| p.buy_usd().sats_from_cents(cents))
-            .await?;
+            .await?
+            .ceil();
         let sats_after_fee = self
             .fee_calculator
             .increase_by_fee(immediate_execution, sats)
             .ceil();
-        let sats_spread = (sats_after_fee - sats).floor();
+        let sats_spread = sats_after_fee - sats;
         let cents_spread = cents_spread(cents, sats, sats_after_fee);
         Ok(ConversionResult {
             sats: sats_after_fee,
@@ -90,12 +93,13 @@ impl PriceCalculator {
         let cents = self
             .price_mixer
             .apply(|p| p.sell_usd().cents_from_sats(sats))
-            .await?;
+            .await?
+            .ceil();
         let cents_after_fee = self
             .fee_calculator
             .increase_by_fee(immediate_execution, cents)
             .ceil();
-        let cents_spread = (cents_after_fee - cents).floor();
+        let cents_spread = cents_after_fee - cents;
         let sats_spread = sats_spread(sats, cents, cents_after_fee);
         Ok(ConversionResult {
             sats,
@@ -113,12 +117,13 @@ impl PriceCalculator {
         let sats = self
             .price_mixer
             .apply(|p| p.sell_usd().sats_from_cents(cents))
-            .await?;
+            .await?
+            .floor();
         let sats_after_fee = self
             .fee_calculator
             .decrease_by_fee(immediate_execution, sats)
             .floor();
-        let sats_spread = (sats_after_fee - sats).floor();
+        let sats_spread = sats_after_fee - sats;
         let cents_spread = cents_spread(cents, sats, sats_after_fee);
         Ok(ConversionResult {
             sats: sats_after_fee,
@@ -181,37 +186,53 @@ mod tests {
             .cents_from_sats_for_buy(Satoshis::from(dec!(100_000_000)), true)
             .await?;
         assert_eq!(res.cents, UsdCents::from(dec!(98_900)));
+        assert_eq!(res.cents_spread, UsdCents::from(dec!(-1_100)));
+        assert_eq!(res.sats_spread, Satoshis::from(dec!(1_112_234)));
         let res = calc
             .cents_from_sats_for_buy(Satoshis::from(Decimal::ONE), true)
             .await?;
         assert_eq!(res.cents, UsdCents::from(Decimal::ZERO));
+        assert_eq!(res.cents_spread, UsdCents::from(dec!(0)));
+        assert_eq!(res.sats_spread, Satoshis::from(dec!(0)));
 
         let res = calc
             .cents_from_sats_for_buy(Satoshis::from(dec!(100_000_000)), false)
             .await?;
         assert_eq!(res.cents, UsdCents::from(dec!(89_900)));
+        assert_eq!(res.cents_spread, UsdCents::from(dec!(-10_100)));
+        assert_eq!(res.sats_spread, Satoshis::from(dec!(11_234_705)));
         let res = calc
             .cents_from_sats_for_buy(Satoshis::from(Decimal::ONE), false)
             .await?;
         assert_eq!(res.cents, UsdCents::from(Decimal::ZERO));
+        assert_eq!(res.cents_spread, UsdCents::from(dec!(0)));
+        assert_eq!(res.sats_spread, Satoshis::from(dec!(0)));
 
         let res = calc
             .sats_from_cents_for_buy(UsdCents::from(dec!(1_000_000)), true)
             .await?;
         assert_eq!(res.sats, Satoshis::from(dec!(1_011_000_000)));
+        assert_eq!(res.cents_spread, UsdCents::from(dec!(-10_881)));
+        assert_eq!(res.sats_spread, Satoshis::from(dec!(11_000_000)));
         let res = calc
             .sats_from_cents_for_buy(UsdCents::from(Decimal::ONE), true)
             .await?;
         assert_eq!(res.sats, Satoshis::from(dec!(1_011)));
+        assert_eq!(res.cents_spread, UsdCents::from(dec!(-1)));
+        assert_eq!(res.sats_spread, Satoshis::from(dec!(11)));
 
         let res = calc
             .sats_from_cents_for_buy(UsdCents::from(dec!(1_000_000)), false)
             .await?;
         assert_eq!(res.sats, Satoshis::from(dec!(1_101_000_000)));
+        assert_eq!(res.cents_spread, UsdCents::from(dec!(-91_735)));
+        assert_eq!(res.sats_spread, Satoshis::from(dec!(101_000_000)));
         let res = calc
             .sats_from_cents_for_buy(UsdCents::from(Decimal::ONE), false)
             .await?;
         assert_eq!(res.sats, Satoshis::from(dec!(1_101)));
+        assert_eq!(res.cents_spread, UsdCents::from(dec!(-1)));
+        assert_eq!(res.sats_spread, Satoshis::from(dec!(101)));
 
         Ok(())
     }
@@ -223,44 +244,60 @@ mod tests {
             .cents_from_sats_for_sell(Satoshis::from(dec!(100_000_000)), true)
             .await?;
         assert_eq!(res.cents, UsdCents::from(dec!(1_011_000)));
+        assert_eq!(res.cents_spread, UsdCents::from(dec!(11_000)));
+        assert_eq!(res.sats_spread, Satoshis::from(dec!(-1_088_032)));
         let res = calc
             .cents_from_sats_for_sell(Satoshis::from(Decimal::ONE), true)
             .await?;
-        assert_eq!(res.cents, UsdCents::from(Decimal::ONE));
+        assert_eq!(res.cents, UsdCents::from(dec!(2)));
+        assert_eq!(res.cents_spread, UsdCents::from(dec!(1)));
+        assert_eq!(res.sats_spread, Satoshis::from(dec!(-1)));
 
         let res = calc
             .cents_from_sats_for_sell(Satoshis::from(dec!(100_000_000)), false)
             .await?;
         assert_eq!(res.cents, UsdCents::from(dec!(1_101_000)));
+        assert_eq!(res.cents_spread, UsdCents::from(dec!(101_000)));
+        assert_eq!(res.sats_spread, Satoshis::from(dec!(-9_173_479)));
         let res = calc
             .cents_from_sats_for_sell(Satoshis::from(Decimal::ONE), false)
             .await?;
-        assert_eq!(res.cents, UsdCents::from(Decimal::ONE));
+        assert_eq!(res.cents, UsdCents::from(dec!(2)));
+        assert_eq!(res.cents_spread, UsdCents::from(dec!(1)));
+        assert_eq!(res.sats_spread, Satoshis::from(dec!(-1)));
 
         let res = calc
             .sats_from_cents_for_sell(UsdCents::from(dec!(1_000_000)), true)
             .await?;
         assert_eq!(res.sats, Satoshis::from(dec!(98_900_000)));
+        assert_eq!(res.cents_spread, UsdCents::from(dec!(11_122)));
+        assert_eq!(res.sats_spread, Satoshis::from(dec!(-1_100_000)));
         let res = calc
             .sats_from_cents_for_sell(UsdCents::from(Decimal::ONE), true)
             .await?;
         assert_eq!(res.sats, Satoshis::from(dec!(98)));
+        assert_eq!(res.cents_spread, UsdCents::from(dec!(0)));
+        assert_eq!(res.sats_spread, Satoshis::from(dec!(-2)));
 
         let res = calc
             .sats_from_cents_for_sell(UsdCents::from(dec!(1_000_000)), false)
             .await?;
         assert_eq!(res.sats, Satoshis::from(dec!(89_900_000)));
+        assert_eq!(res.cents_spread, UsdCents::from(dec!(112_347)));
+        assert_eq!(res.sats_spread, Satoshis::from(dec!(-10_100_000)));
         let res = calc
             .sats_from_cents_for_sell(UsdCents::from(Decimal::ONE), false)
             .await?;
         assert_eq!(res.sats, Satoshis::from(dec!(89)));
+        assert_eq!(res.cents_spread, UsdCents::from(dec!(0)));
+        assert_eq!(res.sats_spread, Satoshis::from(dec!(-11)));
 
         Ok(())
     }
 
     #[test]
     fn test_sats_spread() {
-        let sats = Satoshis::from(dec!(1000));
+        let sats = Satoshis::from(dec!(1_000));
         let cents = UsdCents::from(dec!(50));
         let cents_after_fee = UsdCents::from(dec!(45));
         let res = sats_spread(sats, cents, cents_after_fee);
@@ -276,8 +313,8 @@ mod tests {
     #[test]
     fn test_cents_spread() {
         let cents = UsdCents::from(dec!(50));
-        let sats = Satoshis::from(dec!(1000));
-        let sats_after_fee = Satoshis::from(dec!(1111));
+        let sats = Satoshis::from(dec!(1_000));
+        let sats_after_fee = Satoshis::from(dec!(1_111));
         let res = cents_spread(cents, sats, sats_after_fee);
         assert_eq!(res, UsdCents::from(dec!(-5)));
 
