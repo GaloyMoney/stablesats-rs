ba4697c9721bbc4842033a35eeceddfe717e24e0
bodymindarts <justin@galoy.io>
2024-01-02 11:20:39 +0100

    test: conditionally execute galoy-client test

--- galoy-client/tests/galoy_client.rs
+++ galoy-client/tests/galoy_client.rs
@@ -2,28 +2,27 @@ use std::env;
 
 use galoy_client::*;
 
-fn client_configuration() -> GaloyClientConfig {
-    let api = env::var("GALOY_GRAPHQL_URI").expect("GALOY_GRAPHQL_URI not set");
-    let phone_number = env::var("GALOY_PHONE_NUMBER").expect("GALOY_PHONE_NUMBER not set");
-    let code = env::var("GALOY_PHONE_CODE").expect("GALOY_PHONE_CODE not set");
+async fn configured_client() -> anyhow::Result<GaloyClient> {
+    let api = env::var("GALOY_GRAPHQL_URI")?;
+    let phone_number = env::var("GALOY_PHONE_NUMBER")?;
+    let code = env::var("GALOY_PHONE_CODE")?;
 
-    let config = GaloyClientConfig {
+    let client = GaloyClient::connect(GaloyClientConfig {
         api,
         phone_number,
         auth_code: code,
-    };
+    })
+    .await?;
 
-    config
+    Ok(client)
 }
 
 /// Test to get transactions list of the default wallet
 #[tokio::test]
 async fn transactions_list() -> anyhow::Result<()> {
-    let config = client_configuration();
-    let client = GaloyClient::connect(config).await?;
-
-    let transactions = client.transactions_list(None).await?;
-    assert!(transactions.list.len() > 0);
-
+    if let Ok(client) = configured_client().await {
+        let transactions = client.transactions_list(None).await?;
+        assert!(transactions.list.len() > 0);
+    }
     Ok(())
 }
